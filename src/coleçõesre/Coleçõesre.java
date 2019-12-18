94f254e1d394d9b9804c9a44ec6e32b69dffb507
nmmuchate <nicolas.muchate@gmail.com>
2019-12-18 08:33:08 +0200

    Initial commit

---
+++ src/coleçõesre/Coleçõesre.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coleçõesre;
+import java.util.*;
+/**
+ *
+ * @author Vagabundo
+ */
+public class Coleçõesre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+         List l = new ArrayList();
+        ArrayList<String> al = new ArrayList<String>(5);
+        Vector v = new Vector(5, 5);
+        
+        // a)
+        v.add(v);
+        
+        // b) 
+        
+        al.add("31 anos");
+        
+        // c)
+        al.add("10");
+             
+        // d)        
+        al.clear();
+        
+        // e)
+        ArrayList<Integer> ss = new ArrayList<Integer>();
+        
+        // f)
+        ss.add(45);
+        
+        // g)
+        
+        lista.add();
+        
+        //h)
+        Vector<Double> dar = new Vector <Double>();
+        
+        // i)
+        dar.add(Double.NaN);
+        
+        // j)
+        dar = new Vector<>();
+             
+                
+        // k)
+        dar.clear();
+        
+        // l)
+        v.add(v);
+        
+        // m)
+        ss.addAll(v);
+        
+        // n)
+        ss.isEmpty();
+             
+        // o)
+        al.remove(v);
+        
+        // p)
+        al.indexOf(v);
+        
+        // q)
+        al.size();
+        
+        //r)
+     
+        l.removeAll(al);
+        
+        
+        //s)
+        
+        
+        // t)
+        meuArray.rem;
+        
+        // u)
+        Arrays.asList(al);
+        
+        // v)
+        Arrays.toString(ss.toArray());
+        
+        // w)
+        ArrayList lk = new ArrayList();
+        
+        // x)
+        Vector LKC = new Vector();
+        
+        // y)
+       LKC.pop();
+        
+        
+        // y)
+        lk.clear();
+        
+        
+        // z)
+        lk.clear();
+        
+    }
+    
+}
+
+    }
+    
+}
